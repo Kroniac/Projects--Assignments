0b593a7dd2f543830072eefad0cb3b19169f55d4
Farid Ansari <farid12ansari7@gmail.com>
2017-11-12 03:16:30 +0530

    404 page added for unknown routes

--- react-assignment-3-routing/src/containers/MainPage/MainPage.js
+++ react-assignment-3-routing/src/containers/MainPage/MainPage.js
@@ -1,25 +1,30 @@
 import React, { Component } from "react";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import Courses from "../Courses/Courses";
 import Users from "../Users/Users";
-import "./MainPage.css"
-import Course from "../Course/Course"
+import "./MainPage.css";
 
 class MainPage extends Component {
   render() {
     return (
       <div>
-          <header>
-            <nav>
-        <ul>
-          <li><NavLink to="/courses">Courses</NavLink></li>
-          <li><NavLink to="/users">Users</NavLink></li>
-        </ul>
-        </nav>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/courses">Courses</NavLink>
+              </li>
+              <li>
+                <NavLink to="/users">Users</NavLink>
+              </li>
+            </ul>
+          </nav>
         </header>
-        <Route path="/courses" component={Courses} />
-        <Route path="/users" component={Users} />
-        
+        <Switch>
+          <Route path="/courses" component={Courses} />
+          <Route path="/users" component={Users} />
+          <Route render={()=><h1>Page Not Found</h1>} />
+        </Switch>
       </div>
     );
   }
