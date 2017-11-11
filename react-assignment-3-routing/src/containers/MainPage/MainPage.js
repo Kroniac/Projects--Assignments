182f754233df7f134c63311f8d749b9098691da7
Farid Ansari <farid12ansari7@gmail.com>
2017-11-11 20:49:23 +0530

    assignment start setup

---
+++ react-assignment-3-routing/src/containers/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react';
+import Courses from '../Courses/Courses';
+import Users from '../Users/Users';
+
+class MainPage extends Component {
+ render(){
+     return(
+       <div />
+     )
+ }
+}
+
+export default MainPage;
